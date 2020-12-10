3bc4ecdedd70b9b7bca4253638e25f9b96caf443
iamsean <40333515@live.napier.ac.uk>
2020-12-10 18:13:15 +0000

    removed unneeded cra default files, installed node-sass and created home component

---
+++ src/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='homepage'>
+      <div className='menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='sub-title'>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
