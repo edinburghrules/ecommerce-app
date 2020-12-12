ac4c4ff5ea4bdcda35c3475fcfee727f34013631
iamsean <40333515@live.napier.ac.uk>
2020-12-12 14:07:24 +0000

    added slider styles

--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -5,9 +5,44 @@ class Slider extends React.Component {
   render() {
     return (
       <div className='slider'>
-        <h1>SLIDER</h1>
+        <div className='slider__list-container'>
+          <ul className='slider__list'>
+            <li className='slider__list-item'>
+              <img
+                className='slider__img'
+                src='/assets/hero1.jpg'
+                alt='hero1'
+              />
+            </li>
+            <li className='slider__list-item'>
+              <img
+                className='slider__img'
+                src='/assets/hero2.jpg'
+                alt='hero2'
+              />
+            </li>
+            <li className='slider__list-item'>
+              <img
+                className='slider__img'
+                src='/assets/hero3.jpg'
+                alt='hero3'
+              />
+            </li>
+          </ul>
+        </div>
+        <div className='slider__buttons'>
+          <button className='slider__button slider__button--prev'>
+            previous
+          </button>
+          <button className='slider__button slider__button--next'>next</button>
+        </div>
+        <div className='slider__nav'>
+          <button className='slider__nav-item'></button>
+          <button className='slider__nav-item'></button>
+          <button className='slider__nav-item'></button>
+        </div>
       </div>
-    )
+    );
   }
 }
 
