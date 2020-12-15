8ea020a5501b71a87e23507175c247d70d45d23f
iamsean <40333515@live.napier.ac.uk>
2020-12-15 10:41:37 +0000

    added gover style to main menu

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -6,7 +6,9 @@ const MenuItem = ({ category, src, link, history }) => {
   return (
     <div onClick={() => history.push(`/category/${link}`)} className='menu-item'>
       <div className='menu-item__content'>
+      <div className='menu-item__img-wrapper'>
         <img src={src} alt={`${link}`} />
+      </div>  
         <h1 className='menu-item__title'>{category}</h1>
       </div>
     </div>
