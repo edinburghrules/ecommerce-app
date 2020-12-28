6109d88651c149300113a1684946603a3394d5ef
iamsean <40333515@live.napier.ac.uk>
2020-12-28 16:47:41 +0000

    Connected private route component to redux passed authenticated state to props.

--- src/utils/private-route/PrivateRoute.jsx
+++ src/utils/private-route/PrivateRoute.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-
-
+import { connect } from 'react-redux';
 
 const PrivateRoute = ({ authenticated, component: Component, ...rest }) => (
   <Route
     {...rest}
-    render={(props) => (authenticated ? <Redirect to='/' /> : <Component {...props} />)}
+    render={(props) =>
+      authenticated ? <Redirect to='/' /> : <Component {...props} />
+    }
   />
 );
 
-export default PrivateRoute;
+const mapStateToProps = (state) => {
+  console.log(state.account.authenticated);
+  return {
+    authenticated: state.account.authenticated,
+  };
+};
+
+export default connect(mapStateToProps)(PrivateRoute);
